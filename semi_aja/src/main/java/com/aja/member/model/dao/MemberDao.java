4e565790563ad9ef0f8a05a6a95d1474c69507e1
uoahir <choco616@naver.com>
2024-05-19 00:45:57 +0900

    519

--- semi_aja/src/main/java/com/aja/member/model/dao/MemberDao.java
+++ semi_aja/src/main/java/com/aja/member/model/dao/MemberDao.java
@@ -9,11 +9,14 @@ import java.util.Properties;
 
 import com.aja.member.model.dto.Customer;
 
+import static com.aja.common.JDBCTemplate.*;
+
 public class MemberDao {
 	
 	private Properties prop = new Properties();
 	
 	{
+		// 프로펄티즈 파일 내 쿼리문 변경될 때, dao 로직 실행 시 프로펄티즈 파일을 재로드해서 실행하기 때문에 유지보수성 향상됨
 		String path = MemberDao.class.getResource("").getPath();
 		try (FileReader fr = new FileReader(path)){
 			
@@ -28,6 +31,7 @@ public class MemberDao {
 		PreparedStatement pstmt = null;
 		int result = 0;
 		String sql = "INSERT INTO MEMBER VALUES(CK_SEQ.NEXTVAL, ?, ?, ?, ?, ?, ?, ?, ?, DEFAULT, DEFAULT)";
+		// 리소스파일(프로펄티즈파일) 만들어서 넣기 ~
 		try {
 			pstmt = conn.prepareStatement(sql);
 			pstmt.setString(1, ct.getCustEmail());
@@ -44,7 +48,7 @@ public class MemberDao {
 		} catch(SQLException e) {
 			e.printStackTrace();
 		} finally {
-			
+			close(pstmt);
 		}
 		
 		return result;
