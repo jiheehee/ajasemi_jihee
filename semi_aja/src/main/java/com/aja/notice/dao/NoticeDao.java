98922fb1832962a8698cbfb84efc8bddeef02a6c
youjh <youjh@LAPTOP-REVK6GLB>
2024-05-20 17:19:56 +0900

    jh_NoticeDao_commit

--- semi_aja/src/main/java/com/aja/notice/dao/NoticeDao.java
+++ semi_aja/src/main/java/com/aja/notice/dao/NoticeDao.java
@@ -1,5 +1,7 @@
 package com.aja.notice.dao;
 
+import static com.aja.common.JDBCTemplate.close;
+
 import java.io.FileReader;
 import java.io.IOException;
 import java.sql.Connection;
@@ -11,8 +13,6 @@ import java.util.List;
 import java.util.Properties;
 
 import com.aja.notice.dto.Notice;
-
-import static com.aja.common.JDBCTemplate.close;
 
 // 공지사항 목록 조회하는 로직 NoticeDao 
 // 전체 쿼리를 반환받아 List<Notice>로 저장하고 반환한다. 
@@ -50,8 +50,54 @@ public class NoticeDao {
 		}return result;		
 	}
 	
+	public int selectNoticeCount(Connection conn) {
+		PreparedStatement pstmt=null;
+		ResultSet rs=null;
+		int result=0;
+		try {
+			pstmt=conn.prepareStatement(sql.getProperty("selectNoticeCount"));
+			rs=pstmt.executeQuery();
+			rs.next();
+		}catch(SQLException e) {
+			e.printStackTrace();
+		}finally {
+			close(rs);
+			close(pstmt);
+		}return result;		
+	}
 	
+	public Notice selectNoticeByNo(Connection conn,int no) {
+		PreparedStatement pstmt=null;
+		ResultSet rs=null;
+		Notice n=null;
+		try {
+			pstmt=conn.prepareStatement(sql.getProperty("selectNoticeByNo"));
+			pstmt.setInt(1, no);
+			rs=pstmt.executeQuery();
+			if(rs.next()) {
+				n=getNotice(rs);
+			}					
+		}catch(SQLException e) {
+			e.printStackTrace();
+		}finally {
+			close(rs);
+			close(pstmt);
+		}return n;		
+	}
 	
+	public int insertNotice(Connection conn, Notice n) {
+		PreparedStatement pstmt=null;
+		int result=0;
+		try {
+			pstmt=conn.prepareStatement(sql.getProperty("insertNotice"));
+			pstmt.setString(1,n.getNoticeTitle());
+			pstmt.setDate(2,n.getNoticeEnrolldate());			
+		}catch(SQLException e) {
+			e.printStackTrace();
+		}finally {
+			close(pstmt);
+		}return result;		
+	}		
 	
 	public static Notice getNotice(ResultSet rs) throws SQLException{
 		return Notice.builder()
